cef326b4ca9b4bab6a351b5721700b5c08fbb312
ILLUhS <illuhs@yandex.ru>
2022-11-02 20:40:03 +0300

    Update freecodecamp.js
    new task

--- freecodecamp.js
+++ freecodecamp.js
@@ -4,3 +4,7 @@ var removedFromMyArray = myArray.pop();
 // Only change code below this line
 
 
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+var removedFromMyArray = myArray.shift();
+// Only change code below this line
