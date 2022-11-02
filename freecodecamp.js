1b991c9659500989a42698930954c25c16985fbf
ILLUhS <illuhs@yandex.ru>
2022-11-02 20:43:14 +0300

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -8,3 +8,10 @@ var removedFromMyArray = myArray.pop();
 const myArray = [["John", 23], ["dog", 3]];
 var removedFromMyArray = myArray.shift();
 // Only change code below this line
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+// Only change code below this line
