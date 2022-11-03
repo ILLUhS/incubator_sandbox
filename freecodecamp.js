90e604ab4de35993c6693edc1ba4d53f2959b390
ILLUhS <illuhs@yandex.ru>
2022-11-03 11:40:08 +0300

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -315,3 +315,88 @@ function testElse(val) {
 }
 
 testElse(4);
+
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else{
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+
+function testSize(num) {
+  // Only change code below this line
+  if(num < 5){
+    return "Tiny";
+  }
+  else if(num < 10){
+    return "Small";
+  }
+  else if(num < 15){
+    return "Medium";
+  }
+  else if(num < 20){
+    return "Large";
+  }
+  else if(num >= 20){
+    return "Huge";
+  }
+  return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
+
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1){
+    return "Hole-in-one!";
+  }
+  else if(strokes <= par - 2){
+    return "Eagle";
+  }
+  else if(strokes == par - 1){
+    return "Birdie";
+  }
+  else if(strokes == par){
+    return "Par";
+  }
+  else if(strokes == par + 1){
+    return "Bogey";
+  }
+  else if(strokes == par + 2){
+    return "Double Bogey";
+  }
+  else if(strokes >= par + 3){
+    return "Go Home!";
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
