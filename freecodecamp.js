ff1e00fdb1942a7e63a27c80f6f823589bb06ff6
ILLUhS <illuhs@yandex.ru>
2022-11-03 11:12:04 +0300

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -176,3 +176,14 @@ function testStrict(val) {
 }
 
 testStrict(10);
+
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
