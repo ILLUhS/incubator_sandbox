f355ccc2ba8d47527af0166735850d11f311d960
ILLUhS <illuhs@yandex.ru>
2022-11-03 11:19:50 +0300

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -254,3 +254,64 @@ function testGreaterOrEqual(val) {
 }
 
 testGreaterOrEqual(10);
+
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);+
+
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10 ) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
