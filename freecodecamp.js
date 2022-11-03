a1d215fdb8cdbe9ff791916d461b4fa3a82d2770
ILLUhS <illuhs@yandex.ru>
2022-11-03 12:25:50 +0300

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -597,3 +597,27 @@ const myDog = {
 
   // Only change code above this line
 };
+
+
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
